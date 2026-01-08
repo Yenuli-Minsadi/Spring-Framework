977dba8037db8b7f6a7815962129c210400939ea
Yenuli Minsadi <samararatney@gmail.com>
2026-01-08 13:34:07 +0530

    update with to get bean id by calling base package classes

--- SpringFramework73/01_Spring_Core/src/main/java/edu/config/AppConfig.java
+++ SpringFramework73/01_Spring_Core/src/main/java/edu/config/AppConfig.java
@@ -1,10 +1,12 @@
 package edu.config;
 
+import edu.newBeans.NewTestBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration//component
 @ComponentScan(basePackages= {"edu.bean", "edu.newBeans"})// scan and link all beans in the application with bean path, spring bean/components okkoma meken aran awilla
+//@ComponentScan(basePackageClasses = NewTestBean.class)
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig is created");
